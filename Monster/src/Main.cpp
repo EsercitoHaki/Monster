ae5339cc2ff4bed3e2fe2f5cab4c8fb73e750e0d
EsercitoHaki <esercitohaki2003@gmail.com>
2025-08-05 22:46:23 +0700

    draw square

--- Monster/src/Main.cpp
+++ Monster/src/Main.cpp
@@ -59,25 +59,37 @@ int main() {
 	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
+		 0.5f, 0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
 		 -0.5f, -0.5f, 0.0f,
-		  0.5f, -0.5f, 0.0f,
-		  0.0f,  0.5f, 0.0f
+		 -0.5f, 0.5f, 0.0f
 	};
 
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
 
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	/*glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);*/
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -92,7 +104,10 @@ int main() {
 
 		glBindVertexArray(VAO);
 		
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		/*glDrawArrays(GL_TRIANGLES, 0, 3);*/
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -100,6 +115,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
